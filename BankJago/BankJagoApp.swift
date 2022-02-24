a00978d67cee44345e3645bb021ab91082bbc1bc
andes2912 <andridesmana29@gmail.com>
2022-02-25 00:21:29 +0700

    Initial Commit

---
+++ BankJago/BankJagoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankJagoApp.swift
+//  BankJago
+//
+//  Created by Andri Desmana on 25/02/22.
+//
+
+import SwiftUI
+
+@main
+struct BankJagoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
